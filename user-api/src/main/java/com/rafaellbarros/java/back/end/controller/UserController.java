fc7b67abe3b295440a7ff52f3797e90fb159ad73
Rafael Barros <rafael.bsilva@castgroup.com.br>
2021-03-26 10:16:57 -0300

    :construction: Criando newUser

--- user-api/src/main/java/com/rafaellbarros/java/back/end/controller/UserController.java
+++ user-api/src/main/java/com/rafaellbarros/java/back/end/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.rafaellbarros.java.back.end.controller;
 
 import com.rafaellbarros.java.back.end.model.dto.UserDTO;
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -34,6 +37,13 @@ public class UserController {
             }
         }
         return  null;
+    }
+
+    @PostMapping("/newUser")
+    public UserDTO inserir(@RequestBody UserDTO userDTO) {
+        userDTO.setDataCadastro(new Date());
+        usuarios.add(userDTO);
+        return userDTO;
     }
 
     @PostConstruct
